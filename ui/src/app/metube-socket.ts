9907e1b8851b2bc491592697533ea6242b58cc80
Alex Shnitman <alexta69@gmail.com>
2025-12-05 11:24:44 +0200

    upgrade to angular 20

--- ui/src/app/metube-socket.ts
+++ ui/src/app/metube-socket.ts
@@ -1,11 +1,12 @@
 import { Injectable } from '@angular/core';
+import { ApplicationRef } from '@angular/core';
 import { Socket } from 'ngx-socket-io';
 
 @Injectable()
 export class MeTubeSocket extends Socket {
-  constructor() {
+  constructor(appRef: ApplicationRef) {
     const path =
       document.location.pathname.replace(/share-target/, '') + 'socket.io';
-    super({ url: '', options: { path } });
+    super({ url: '', options: { path } }, appRef);
   }
 }
